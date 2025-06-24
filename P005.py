b2a8925c2d9908b8370a21f1002dc614adb5d3b9
Ignacio Ortiz <85143684+tomahawk360@users.noreply.github.com>
2025-06-24 02:33:19 -0400

    Update P005

--- P005.py
+++ P005.py
@@ -8,14 +8,21 @@ import tracemalloc
 from ttp import ttp
 
 
-""" 
-    ------- Observaciones -------
-    En esta etapa se identifican los tiempos de inicio de las observaciones del dia correspondiente.
-
-    Retorna: 
-        - tpl_list: str[] -> Lista de tuplas, con el id de la observacion primero y el tiempo de inicio de la observacion segundo.
 """
+Global variables:
+    tracking_time: Initialization of time for checkpoint monitoring
+"""
+tracking_time = time.time()
 
+"""
+Opens the VLT Observations CSV file and filters out the header and irrelevant fields.
+
+Args:
+    arch_name: CSV file relative path.
+
+Returns:
+    A list of tuples, with each tuple containing the observation ID and it's respective timestamp.
+"""
 def open_obs_file(arch_name):
     tpl_list = []
 
@@ -29,7 +36,15 @@ def open_obs_file(arch_name):
 
     return tpl_list
 
+"""
+Opens a text file and deposits it's information into a list.
 
+Args:
+    arch_name: Text file relative path.
+
+Returns:
+    A list with all the lines as strings.
+"""
 def open_txt_file(arch_name):
     with open(file=arch_name, mode='r', encoding='iso-8859-1') as logs:
         lines = logs.readlines()
@@ -37,33 +52,46 @@ def open_txt_file(arch_name):
     return lines
 
 
-""" 
-    Nombre:   
-        header_removal
-    Retorna: 
-        headless_lines: -> Lista de listas de tuplas (index, linea sin cabecera), con cada lista correspondiente a las lineas
-        entre el fin de un ciclo de autoguider del VLT y el inicio del siguiente ciclo de autoguider.
 """
-def header_removal(logger, log_lines):
+Removes the header of each log line and groups them in blocks, according the timeframe between the VLT autoguider stopping an iteration
+and the beggining of the following iteration, which corresponds to an observation period.
+
+Args:
+    logger: Current script logging object.
+    log_lines: List of log lines, in string form.
+
+Returns:
+    A list of lists, with each containing a group of headerless log lines, fitted by observation period.
+"""
+def log_pre_processing(logger, log_lines):
+    global tracking_time
+
+    ### Checkpoint - Start log pre-processing
+    logger.info('Start log pre-processing: {0}'.format(str(time.time() - tracking_time)))
+    tracking_time = time.time()
+    
     headerless_lines = []
     extraction_flag = False
 
     for index, line in enumerate(log_lines):
         
-        #Eliminación del header del log
+        # Log line header removal
         header_re = re.compile(r'([a-zA-Z]{3}\s[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}?)\swt1tcs\s(.+)(\[[0-9]+\]):\s')
 
         headerless_line = re.sub(header_re, '', line)
 
         if(headerless_line.find("AG.GUIDE") != -1):
+            ### Checkpoint - AG.GUIDE line found
+            logger.info('AG.GUIDE line found: {0}'.format(str(time.time() - tracking_time)))
+            tracking_time = time.time()
 
             if(headerless_line.find("STOP") != -1):
-                # Comenzar extraccion de lineas de log
+                # Start log line extraction
                 headerless_lines.append([])
                 extraction_flag = True
 
             elif(headerless_line.find("START") != -1):
-                # Terminar extraccion de lineas de log
+                # Finish log line extraction
                 if(len(headerless_lines) != 0):
                     headerless_lines[-1].append((index, headerless_line))
 
@@ -73,19 +101,38 @@ def header_removal(logger, log_lines):
             headerless_line = headerless_line.replace("  ", " ")
             headerless_lines[-1].append((index, headerless_line))
 
+            ### Checkpoint - Line added to current list
+            logger.info('Line added to current list: {0}'.format(str(time.time() - tracking_time)))
+            tracking_time = time.time()
+
+    ### Checkpoint - End log pre-processing
+    logger.info('End log pre-processing: {0}'.format(str(time.time() - tracking_time)))
+    tracking_time = time.time()
+
     return headerless_lines
 
 
-""" 
-    Nombre:   
-        obs_filtering
-    Retorna: 
-        obs_log_sections: -> Lista de listas de tuplas (index, linea sin cabecera), cada lista perteneciente a una observacion.
+"""
+Checks a list with observation periods' log lines and filters out those groups where an observation was not successful.
+
+Args:
+    logger: Current script logging object.
+    log_lines: List of lists of log lines, grouped by observation period, in string form.
+    obs_list: List of tuples, with observation IDs and observation timestamp, in string form.
+
+Returns:
+    A list of lists, with each containing a group of headerless log lines, fitted by observation period with successful observations.
 """
 def obs_filtering(logger, log_lines, obs_list):
+    global tracking_time
+
+    ### Checkpoint - Start observation log lines filtering
+    logger.info('Start observation log lines filtering: {0}'.format(str(time.time() - tracking_time)))
+    tracking_time = time.time()
+            
     obs_log_sections = []
 
-    #Revisar que grupos de lineas de log calzan a una observacion
+    #Check which log line groups match their timeframes with an observation timestamp
     for log_section in log_lines:
 
         stop_line = log_section[0][1]
@@ -99,22 +146,34 @@ def obs_filtering(logger, log_lines, obs_list):
         for tpl_id, tpl_time in obs_list:
 
             if(stop_time < tpl_time and tpl_time < start_time):
-                logger.info("*** TPL ID: " + tpl_id)
+                ### Checkpoint - Line within successful observation timeframe found
+                logger.info('Line within successful observation timeframe found: {0}'.format(str(time.time() - tracking_time)))
+                tracking_time = time.time()
 
-                #Habilitar parseo de seccion de log
+                #Save log line group
                 obs_log_sections.append(log_section)
                 break
+
+    ### Checkpoint - End observation log lines filtering
+    logger.info('End observation log lines filtering: {0}'.format(str(time.time() - tracking_time)))
+    tracking_time = time.time()
 
     return obs_log_sections
 
 
-""" 
-    Nombre:   
-        template_generator
-    Retorna: 
-        : -> 
+"""
+In Development
+
+Args:
+
+Returns:
+
 """
 def template_generator(logger, templates_list):
+    ### Checkpoint
+    logger.info('Inicio de generador de plantillas: {0}'.format(str(time.time() - tracking_time)))
+    tracking_time = time.time()
+
     nu_template_list = []
 
     for template in templates_list:
@@ -134,65 +193,158 @@ def template_generator(logger, templates_list):
     return nu_template_list
 
 
-""" 
-    Nombre:   
-        log_parsing
-    Retorna: 
-        parsed_data: -> Lista de jsons con los datos extraidos de las lineas de log.
 """
-def log_parsing(logger, log_sections, templates_list):
+Parsess each log line and extracts their dynamic data, using the Text Template Parsing library (ttp).
+
+Args:
+    logger: Current script logging object.
+    log_sections: List of lists of log lines, grouped by successful observation period, in string form.
+    templates_list: List of log line parsing templates, in string form.
+
+Returns:
+    A list of dictionaries, with each containing the dynamic data extracted from a log line.
+"""
+def log_parsing_ttp(logger, log_sections, templates_list):
+    global tracking_time
+
+    ### Checkpoint - Start log line parsing
+    logger.info('Start log line parsing: {0}'.format(str(time.time() - tracking_time)))
+    tracking_time = time.time()
+
+    parsed_data = []
+
+    for log_section in log_sections:
+        for index, line in log_section:
+
+            #Iterates through all templates
+            for template in templates_list:
+
+                #Log line parsing
+                try:
+                    # Parsing with Text Template Parsing library
+                    parser = ttp(data=line, template=template)
+                    parser.parse()
+                    key = list(parser.result(template='per_input', structure="dictionary"))[0]
+                    result = parser.result(template='per_input', structure="dictionary")[key][0]
+
+                    # If match is true, the data extracted is saved and jumps into the next log line
+                    if(len(result) != 0):
+                        # Save parsing result
+                        result['Num_linea'] = index
+                        parsed_data.append(result)
+                        break
+
+                    ### Checkpoint - Log line parsing attempt
+                    logger.info('Log line parsing attempt: {0}'.format(str(time.time() - tracking_time)))
+                    tracking_time = time.time()
+
+                #Recolecta otros errores sobre la plantilla usada (se revisarán a posteriori)
+                except xml.etree.ElementTree.ParseError as err:
+                    logger.info("ERROR: " + str(err))
+
+            ### Checkpoint - End iteration on template list
+            logger.info('End iteration on template list: {0}'.format(str(time.time() - tracking_time)))
+            tracking_time = time.time()
+
+        ### Checkpoint - End iteration on log line group 
+        logger.info('End iteration on log line group: {0}'.format(str(time.time() - tracking_time)))
+        tracking_time = time.time()
+
+    ### Checkpoint - End log line parsing
+    logger.info('End log line parsing: {0}'.format(str(time.time() - tracking_time)))
+    tracking_time = time.time()
+
+    return parsed_data
+
+
+"""
+Parsess each log line and extracts their dynamic data, using regular expressions.
+
+Args:
+    logger: Current script logging object.
+    log_sections: List of lists of log lines, grouped by successful observation period, in string form.
+    templates_list: List of log line parsing templates, in string form.
+
+Returns:
+    A list of dictionaries, with each containing the dynamic data extracted from a log line.
+"""
+def log_parsing_regex(logger, log_sections, templates_list):
+    global tracking_time
+
+    ### Checkpoint - Start log line parsing
+    logger.info('Start log line parsing: {0}'.format(str(time.time() - tracking_time)))
+    tracking_time = time.time()
+
     parsed_data = []
 
     for log_section in log_sections:
         for index, line in log_section:
             result = {}
 
-            #Marca de LINEA para log
-            logger.info("*** LINEA: " + line)
-
-            #Itera las 14 plantillas
+            #Iterates through all templates
             for template in templates_list:
 
                 template = template.replace("\n", "")
 
-                #Parseo con expresiones regulares
-                try:
-                    parser = re.search(template, line)
+                #Parsing with regular expressions
+                parser = re.search(template, line)
 
-                    #Si hay match con la expresion regular, entonces se parsean los valores dinámicos y se salta a la siguiente linea
-                    if(parser is not None):
+                # If match is true, the data extracted is saved and jumps into the next log line
+                if(parser is not None):
 
-                        logger.info("PLANTILLA: " + template)
+                    parser_res = parser.group()
 
-                        parser_res = parser.group()
+                    # Parses a log line's date
+                    check_date = re.search(r"[0-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]", parser_res)
+                    if(check_date is not None):
+                        result["date_body"] = check_date.group()
+                        parser_res = re.sub(r"[0-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]", '', parser_res)
 
-                        # Guardar como resultado
-                        check_date = re.search(r"[0-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]", parser_res)
-                        if(check_date is not None):
-                            result["date_body"] = check_date.group()
-                            parser_res = re.sub(r"[0-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]", '', parser_res)
+                    # Parsees a log line's hour time
+                    check_time = re.search(r"[0-2][0-9]:[0-5][0-9]:[0-5][0-9](.[0-9]+)*", parser_res)
+                    if(check_time is not None):
+                        result["time_body"] = check_time.group()
+                        parser_res = re.sub(r"[0-2][0-9]:[0-5][0-9]:[0-5][0-9](.[0-9]+)*", '', parser_res)
+                    
+                    # Parsees a log line's numeric data
+                    check_numeric = re.findall(r"\s([0-9.-]+)", parser_res)
+                    if(check_numeric is not None):
+                        result["numberic_values"] = check_numeric
 
-                        check_time = re.search(r"[0-2][0-9]:[0-5][0-9]:[0-5][0-9](.[0-9]+)*", parser_res)
-                        if(check_time is not None):
-                            result["time_body"] = check_time.group()
-                            parser_res = re.sub(r"[0-2][0-9]:[0-5][0-9]:[0-5][0-9](.[0-9]+)*", '', parser_res)
-                        
-                        check_numeric = re.findall(r"[0-9.-]+", parser_res)
-                        if(check_numeric is not None):
-                            result["numberic_values"] = check_numeric
+                    # Save parsing result
+                    result['Num_linea'] = index
+                    parsed_data.append(result)
+                    break
+
+                ### Checkpoint - Log line parsing attempt
+                logger.info('Log line parsing attempt: {0}'.format(str(time.time() - tracking_time)))
+                tracking_time = time.time()
 
 
-                        result['Num_linea'] = index
-                        parsed_data.append(result)
-                        break
+            ### Checkpoint - End iteration on template list
+            logger.info('End iteration on template list: {0}'.format(str(time.time() - tracking_time)))
+            tracking_time = time.time()
 
-                #Recolecta otros errores sobre la plantilla usada (se revisarán a posteriori)
-                except xml.etree.ElementTree.ParseError as err:
-                    logger.info("ERROR: " + str(err))
+        ### Checkpoint - End iteration on log line group 
+        logger.info('End iteration on log line group: {0}'.format(str(time.time() - tracking_time)))
+        tracking_time = time.time()
+
+    ### Checkpoint - End log line parsing
+    logger.info('End log line parsing: {0}'.format(str(time.time() - tracking_time)))
+    tracking_time = time.time()
 
     return parsed_data
 
+"""
+Saves parsed data in an external file
 
+Args:
+    parsed_data: List of dictionaries, each containing the log line dynamic parsed data
+    dest_arch_name: Relative path of file for parsed data
+
+Returns:
+    None
+"""
 def save_parsed_data(parsed_data, dest_arch_name):
     with open(dest_arch_name, "w") as data_file:
         json.dump(parsed_data, data_file)
@@ -200,78 +352,37 @@ def save_parsed_data(parsed_data, dest_arch_name):
 
 #Main
 if __name__ == '__main__':
-    #Logger del MVP
+    #### MVP Logger
     logging.basicConfig(filename='P005.log', level=logging.INFO)
     logger = logging.getLogger('myLogger')
 
-    #Fecha del log
+    ### Log date
     file_name = '2024-09-20'
 
-    #Nombre de archivo de destino para datos parseados
+    ### Relative path of destination file for parsed data
     dest_arch_name = 'P005_data.txt'
 
-    #Csv de observaciones
+    ### Observations csv file extraction
     obs_arch_name = "wdb_query_{0}.csv".format(file_name)
     obs_list = open_obs_file(obs_arch_name)
 
-    #Lineas de log
+    ### Log lines extraction
     log_arch_name = 'logs/wt1tcs.{0}.log'.format(file_name)
     log_lines = open_txt_file(log_arch_name)
 
-    #Plantillas
+    ### Templates extraction
     tplt_arch_name = 'P005_templates.txt'
     tplt_list = open_txt_file(tplt_arch_name)
 
-    #Iniciar monitoreo de memoria 
-    # tracemalloc.start()
-    # init_malloc = tracemalloc.get_traced_memory()
+    #### Log lines pre-processing
+    headerless_logs = log_pre_processing(logger, log_lines)
 
-    #### Remocion de los headers
-    i_time_step_1 = time.time()
-    # i_malloc_1 = tracemalloc.get_traced_memory()
-
-    headerless_logs = header_removal(logger, log_lines)
-
-    # f_malloc_1 = tracemalloc.get_traced_memory()
-    f_time_step_1 = time.time()
-
-    time_step_1 = f_time_step_1 - i_time_step_1
-    # malloc_1 = tuple(map(lambda x, y: x-y, f_malloc_1, i_malloc_1))
-
-    logger.info('Remocion de headers: {0}'.format(str(time_step_1)))
-
-    #### Filtro por logs de observaciones
-    i_time_step_2 = time.time()
-    # i_malloc_2 = tracemalloc.get_traced_memory()
-
+    #### Log lines' observation filtering
     obs_logs = obs_filtering(logger, headerless_logs, obs_list)
 
-    # f_malloc_2 = tracemalloc.get_traced_memory()
-    f_time_step_2 = time.time()
+    #### Log lines parsing
+    parsed_data = log_parsing_regex(logger, obs_logs, tplt_list)
 
-    time_step_2 = f_time_step_2 - i_time_step_2
-    # malloc_2 = tuple(map(lambda x, y: x-y, f_malloc_2, i_malloc_2))
-
-    logger.info('Filtro de lineas de observacion: {0}'.format(str(time_step_2)))
-
-    #### Parsear data de los logs
-    i_time_step_3 = time.time()
-    # i_malloc_3 = tracemalloc.get_traced_memory()
-
-
-    parsed_data = log_parsing(logger, obs_logs, tplt_list)
-
-    # f_malloc_3 = tracemalloc.get_traced_memory()
-    f_time_step_3 = time.time()
-
-    time_step_3 = f_time_step_3 - i_time_step_3
-    # malloc_3 = tuple(map(lambda x, y: x-y, f_malloc_3, i_malloc_3))
-
-    logger.info('Parseo de informacion: {0}'.format(str(time_step_3)))
-
-    #Termino de monitoreo de memoria
-    # tracemalloc.stop()
-
-    #### Guardar data parseada
+    #### Parsed data saving
     save_parsed_data(parsed_data, dest_arch_name)
 
